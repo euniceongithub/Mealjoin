316ae0445e68d173b9cd7e7ba7d4c17b8cb15927
euniceakligo <eyakligo@st.ug.edu.gh>
2025-05-09 22:41:20 +0000

    added get and post route to server.js

--- backend/server.js
+++ backend/server.js
@@ -9,5 +9,35 @@ app.get('/', (req, res) => {
   res.send('MealJoin backend is running');
 });
 
+app.get('/meals', (req, res) => {
+    const meals = [
+      { id: 1, name: 'Spaghetti Bolognese', price: 15 },
+      { id: 2, name: 'Chicken Curry', price: 18 },
+      { id: 3, name: 'Grilled Salmon', price: 20 },
+    ];
+  
+    res.json(meals);
+});
+
+app.post('/meals', (req, res) => {
+    const { name, price } = req.body;
+  
+    if (!name || !price) {
+      return res.status(400).json({ message: 'Name and price are required' });
+    }
+  
+    const newMeal = {
+      id: Date.now(),
+      name,
+      price,
+    };
+  
+    // Database to be added.
+    // Simulating with HTTP request for now.
+  
+    res.status(201).json(newMeal);
+  });
+  
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
